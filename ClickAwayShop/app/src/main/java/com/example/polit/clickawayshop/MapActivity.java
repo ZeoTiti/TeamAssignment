0b3898148ed9ecbe8b6c73336f18e0cfa57db204
ZeoTiti <44121504+ZeoTiti@users.noreply.github.com>
2021-07-19 22:00:20 +0300

    Revert "Update MainActivity.java, MapActivity.java, and 2 more files..."
    This reverts commit 3e2ffbfe697fdfa154c7957ec56b7161ff2efff6.

--- ClickAwayShop/app/src/main/java/com/example/polit/clickawayshop/MapActivity.java
+++ ClickAwayShop/app/src/main/java/com/example/polit/clickawayshop/MapActivity.java
@@ -4,28 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity {
-    FirebaseFirestore mDb;
+    public final String LOG_TAG = "Thanos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mDb = FirebaseFirestore.getInstance();
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                MapUtils.populateMapWithStores(googleMap, mDb.collection("stores"));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(0, 0))
+                        .title("Marker"));
             }
         });
     }
